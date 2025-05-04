2ae5720c2aa5fb735b6f8570777ae00189ab5f3f
Pratoosh-18 <pratoosh10garg@gmail.com>
2025-05-04 18:28:58 +0530

    Commented the context for demo

--- frontend/context/app-provider.tsx
+++ frontend/context/app-provider.tsx
@@ -20,127 +20,130 @@ const AppContext = createContext<null | IAppContext>(null);
 export const useAppContext = () => useContext(AppContext) as IAppContext;
 
 const AppProvider = ({ children }: Readonly<{ children: React.ReactNode }>) => {
-  const [isBackendConnected, setIsBackendConnected] = useState(false);
+  const [isBackendConnected, setIsBackendConnected] = useState(true);
   const [appData, setAppData] = useState<AppData | null>(null);
   const [detections, setDetections] = useState<Detections>(emptyDetections);
-  const wsRef = useRef<WebSocket | null>(null);
 
-  const connectWebSocket = useCallback(() => {
-    const ws = new WebSocket(
-      `ws://${process.env.NEXT_PUBLIC_SERVER_DOMAIN}/ws`
-    );
-    wsRef.current = ws; // Store WebSocket reference in useRef
-    ws.onopen = () => {
-      console.log("WebSocket connection established");
-      setIsBackendConnected(true);
-    };
-    ws.onmessage = (event) => {
-      const data = JSON.parse(event.data);
-      // console.log("Received data:", data);
+  // This section is commented out for the demo since no connection is needed. ( line 30 - 146 )
+  // Uncomment the following part when deploying to production.
 
-      if (data.event === "server:app-data") {
-        setAppData(data.data);
-      }
+  // const wsRef = useRef<WebSocket | null>(null);
+  // const connectWebSocket = useCallback(() => {
+  //   const ws = new WebSocket(
+  //     `ws://${process.env.NEXT_PUBLIC_SERVER_DOMAIN}/ws`
+  //   );
+  //   wsRef.current = ws; // Store WebSocket reference in useRef
+  //   ws.onopen = () => {
+  //     console.log("WebSocket connection established");
+  //     setIsBackendConnected(true);
+  //   };
+  //   ws.onmessage = (event) => {
+  //     const data = JSON.parse(event.data);
+  //     // console.log("Received data:", data);
 
-      if (data.event === "server:red-light-violation") {
-        setDetections((prev) => ({
-          ...prev,
-          redLightPassing: [data.data, ...prev.redLightPassing],
-        }));
-      }
+  //     if (data.event === "server:app-data") {
+  //       setAppData(data.data);
+  //     }
 
-      if (data.event === "server:no-helmet-violation") {
-        setDetections((prev) => ({
-          ...prev,
-          noHelmet: [data.data, ...prev.noHelmet],
-        }));
-      }
+  //     if (data.event === "server:red-light-violation") {
+  //       setDetections((prev) => ({
+  //         ...prev,
+  //         redLightPassing: [data.data, ...prev.redLightPassing],
+  //       }));
+  //     }
 
-      if (data.event === "server:remove-no-helmet-violation") {
-        setDetections((prev) => ({
-          ...prev,
-          noHelmet: prev.noHelmet.filter((item) => item.id !== data.data.id),
-        }));
-      }
+  //     if (data.event === "server:no-helmet-violation") {
+  //       setDetections((prev) => ({
+  //         ...prev,
+  //         noHelmet: [data.data, ...prev.noHelmet],
+  //       }));
+  //     }
 
-      if (data.event === "server:overspeeding") {
-        setDetections((prev) => ({
-          ...prev,
-          overspeeding: [data.data, ...prev.overspeeding],
-        }));
-      }
+  //     if (data.event === "server:remove-no-helmet-violation") {
+  //       setDetections((prev) => ({
+  //         ...prev,
+  //         noHelmet: prev.noHelmet.filter((item) => item.id !== data.data.id),
+  //       }));
+  //     }
 
-      if (data.event === "server:update-overspeeding") {
-        setDetections((prev) => ({
-          ...prev,
-          overspeeding: prev.overspeeding.map((item) =>
-            item.id === data.data.id
-              ? { ...item, highestSpeed: data.data.highestSpeed }
-              : item
-          ),
-        }));
-      }
+  //     if (data.event === "server:overspeeding") {
+  //       setDetections((prev) => ({
+  //         ...prev,
+  //         overspeeding: [data.data, ...prev.overspeeding],
+  //       }));
+  //     }
 
-      if (data.event === "server:pothole") {
-        setDetections((prev) => ({
-          ...prev,
-          pothole: [data.data, ...prev.pothole],
-        }));
-      }
+  //     if (data.event === "server:update-overspeeding") {
+  //       setDetections((prev) => ({
+  //         ...prev,
+  //         overspeeding: prev.overspeeding.map((item) =>
+  //           item.id === data.data.id
+  //             ? { ...item, highestSpeed: data.data.highestSpeed }
+  //             : item
+  //         ),
+  //       }));
+  //     }
 
-      if (data.event === "server:wrong-way") {
-        setDetections((prev) => ({
-          ...prev,
-          wrongWay: [data.data, ...prev.wrongWay],
-        }));
-      }
+  //     if (data.event === "server:pothole") {
+  //       setDetections((prev) => ({
+  //         ...prev,
+  //         pothole: [data.data, ...prev.pothole],
+  //       }));
+  //     }
 
-      if (data.event === "server:vehicle-found") {
-        setDetections((prev) => ({
-          ...prev,
-          vehicleFinder: [data.data, ...prev.vehicleFinder],
-        }));
-      }
+  //     if (data.event === "server:wrong-way") {
+  //       setDetections((prev) => ({
+  //         ...prev,
+  //         wrongWay: [data.data, ...prev.wrongWay],
+  //       }));
+  //     }
 
-      if (data.event === "server:traffic-control") {
-        setDetections((prev) => ({
-          ...prev,
-          trafficControl: [...prev.trafficControl, data.data],
-        }));
-      }
+  //     if (data.event === "server:vehicle-found") {
+  //       setDetections((prev) => ({
+  //         ...prev,
+  //         vehicleFinder: [data.data, ...prev.vehicleFinder],
+  //       }));
+  //     }
 
-      if (data.event === "server:person_detected") {
-        setDetections((prev) => ({
-          ...prev,
-          personDetector: [data.data, ...prev.personDetector],
-        }));
-      }
-    };
-    ws.onerror = (error) => console.error("WebSocket error:", error);
-    ws.onclose = () => {
-      console.log("WebSocket connection closed");
-      setIsBackendConnected(false);
-      wsRef.current = null;
+  //     if (data.event === "server:traffic-control") {
+  //       setDetections((prev) => ({
+  //         ...prev,
+  //         trafficControl: [...prev.trafficControl, data.data],
+  //       }));
+  //     }
 
-      // Attempt to reconnect after a short delay
-      setTimeout(() => {
-        console.log("Reconnecting...");
-        connectWebSocket();
-      }, 1000);
-    };
-    return ws;
-  }, []);
+  //     if (data.event === "server:person_detected") {
+  //       setDetections((prev) => ({
+  //         ...prev,
+  //         personDetector: [data.data, ...prev.personDetector],
+  //       }));
+  //     }
+  //   };
+  //   ws.onerror = (error) => console.error("WebSocket error:", error);
+  //   ws.onclose = () => {
+  //     console.log("WebSocket connection closed");
+  //     setIsBackendConnected(false);
+  //     wsRef.current = null;
 
-  useEffect(() => {
-    const ws = connectWebSocket();
-    wsRef.current = ws;
+  //     // Attempt to reconnect after a short delay
+  //     setTimeout(() => {
+  //       console.log("Reconnecting...");
+  //       connectWebSocket();
+  //     }, 1000);
+  //   };
+  //   return ws;
+  // }, []);
 
-    return () => {
-      if (wsRef.current) {
-        wsRef.current.close();
-      }
-    };
-  }, [connectWebSocket]);
+  // useEffect(() => {
+  //   const ws = connectWebSocket();
+  //   wsRef.current = ws;
+
+  //   return () => {
+  //     if (wsRef.current) {
+  //       wsRef.current.close();
+  //     }
+  //   };
+  // }, [connectWebSocket]);
 
   return (
     <AppContext.Provider value={{ isBackendConnected, appData, detections }}>
